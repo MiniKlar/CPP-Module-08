4ad0d0d60999ce1375704eb5344158b477e344bd
MiniKlar <omontloris@gmail.com>
2026-01-11 19:41:57 +0100

    - Updated main.cpp to correct the test value in easyfind function and improved error message output for clarity

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 23:10:29 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/08 00:36:31 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/11 19:38:51 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	main( void ) {
 		for (size_t i = 0; i < 10; i++) {
 			test.push_back(i);
 		}
-		std::cout << easyfind(test, 12) << std::endl;
+		std::cout << easyfind(test, 9) << std::endl;
 	} catch (std::exception & e) {
-		std::cerr << e.what() << std::endl;
+		std::cerr << "Couldn't find the occurence you tried to search, that was not an easyfind actually!" << std::endl;
 		return (1);
 	}
 	return (0);
